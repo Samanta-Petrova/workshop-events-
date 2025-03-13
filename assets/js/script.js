f39bf364abb7b0fe91ef22bfb4cfab443328f6f4
Samanta-Petrova <samanta0950@gmail.com>
2025-03-13 12:47:00 +0100

    yes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,10 @@
 /*1. Anvende javascript til at skifte farven i de sociale elementer til en mere passende farve (jeres vurdering) */
 // Emelia
-const socialeColor = document.querySelectorAll('.fa');
-for (i = 0; i < socialeColor.length; i++) {
-    socialeColor[i].style.color = '#8d5607';
-}
 //
 
 /*2. Se den lidt skæve h1 i header - sørg for den ved hover "shaker"/ryster sig lidt. I bedes anvende javascript til at løse denne opgave - måske i kombination med css. Der findes endda js libraries/biblioteker I kan se på til formålet, f.eks. anime.js */
 // Debbie
 //
-
-   // Get references to the h1
-        const h1El = document.querySelector('h1');
-   
-        // Add an event listener
-        h1El.addEventListener('mouseover', () => {
-            // Add the 'shake' class to the h1 to trigger the animation
-            h1El.classList.add('shake');
-
-            // Remove the 'shake' class after the animation ends (to allow it to be triggered again)
-            setTimeout(() => {
-                h1El.classList.remove('shake');
-            }, 500); // Duration of the shake animation (500ms)
-        });
 
 /*3. Brug javascript til at skifte billedet ud i .bigPicture-boksen, brug dette eller noget helt andet: https://cdn.pixabay.com/photo/2017/09/28/13/18/amusement-2795490_960_720.jpg */
 // Samanta
@@ -47,42 +29,6 @@ bigPicture.style.background = "https://cdn.pixabay.com/photo/2017/09/28/13/18/am
 //Thomas
 //
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function addIcon (){
-    // opret et <a> tag til icon
-    const favIcon = document.createElement("a");
-    // opret billede tag for iconet (img nu hvor det er link)
-    const hearthstoneIcon = document.createElement("img");
-    // konstasere src for linket fordi noget skal linkes til
-    hearthstoneIcon.src = "https://hearthstoneapi.com/favicon.ico";
-    // Nu tilføj dit billede til dit a tag
-    favIcon.appendChild(hearthstoneIcon);
-    // Hvor skal den placeres? Ja inde i div med class .social
-    const socialDiv = document.querySelector(".social");
-    socialDiv.appendChild(favIcon);
-    // Ændre størrelse på logoet
-    hearthstoneIcon.style.width = "24px";
-    hearthstoneIcon.style.height = "24px";
-}
-addIcon()
-//   
 /*5.1 Brug javascript til at gemme teksterne væk i artiklerne <p>-tags. Indsæt istedet link med "Læs mere..." */
 //
 //
