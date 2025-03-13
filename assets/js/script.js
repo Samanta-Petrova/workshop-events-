100c381d365ca7ccee5c6e7225b1ce5c8a666281
Samanta-Petrova <samanta0950@gmail.com>
2025-03-13 12:39:15 +0100

    idk - ved ikke hvad der ikke virker

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,10 @@
 
 /*3. Brug javascript til at skifte billedet ud i .bigPicture-boksen, brug dette eller noget helt andet: https://cdn.pixabay.com/photo/2017/09/28/13/18/amusement-2795490_960_720.jpg */
 // Samanta
+const imgEl = document.querySelector(".bigPicture");
+bigPicture.style.background = "https://cdn.pixabay.com/photo/2017/09/28/13/18/amusement-2795490_960_720.jpg`)";
+
+
 //
 
 /*4. Brug javascript til at indsætte et favicon - f.eks. det her http://hearthstoneapi.com/favicon.ico (I know...hvorfor ville man nogensinde gøre det irl...men for øvelsens skyld) */
@@ -27,5 +31,4 @@
 //
 
 /*6. Som I måske har lagt mærke til, er der en .crazyPopup box som står udenfor body (se i css...prøv f.eks. at ændre property "right" til value -200 og 0). Opgaven er at den nu "popper up" når personer har scrollet 100px ned og forsvinde igen når de scroller op under 100px igen  - lookie: https://drive.google.com/file/d/10aSqzcL3br8DU5AhggE54Pal0Z3s-x5y/view */
-//
 //
